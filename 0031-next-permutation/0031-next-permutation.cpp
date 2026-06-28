5f87b79c58a90d0df8a5b4af58fb08838d98cc52
Akshat Nigam <akshatnigamdelhi9@gmail.com>
2026-06-28 15:01:10 +0530

    Time: 0 ms (100%), Space: 15.6 MB (93.63%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,28 +1,37 @@
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         int n = nums.size();
+
+//         //  Find pivot
+//         int pivot = -1;
+//         for (int i = n - 2; i >= 0; i--) {
+//             if (nums[i] < nums[i + 1]) {
+//                 pivot = i;
+//                 break;
+//             }
+//         }
+
+//         // If pivot exists, find next greater element
+//         if (pivot != -1) {
+//             for (int i = n - 1; i > pivot; i--) {
+//                 if (nums[i] > nums[pivot]) {
+//                     swap(nums[i], nums[pivot]);
+//                     break;
+//                 }
+//             }
+//         }
+
+//         // Reverse the suffix
+//         reverse(nums.begin() + pivot + 1, nums.end());
+//     }
+// };
+
+
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-
-        //  Find pivot
-        int pivot = -1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                pivot = i;
-                break;
-            }
-        }
-
-        // If pivot exists, find next greater element
-        if (pivot != -1) {
-            for (int i = n - 1; i > pivot; i--) {
-                if (nums[i] > nums[pivot]) {
-                    swap(nums[i], nums[pivot]);
-                    break;
-                }
-            }
-        }
-
-        // Reverse the suffix
-        reverse(nums.begin() + pivot + 1, nums.end());
+        next_permutation(nums.begin(), nums.end());
     }
 };
